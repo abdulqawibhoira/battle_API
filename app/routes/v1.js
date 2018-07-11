c33bf1e50abe213c09a447c889ae2563e862cb3b
Abdul Bhoira <abdulbhoira@BigAdmins-MacBook-Pro-7.local>
2018-07-11 23:55:47 +0530

    csv double quotes removed,API route changed

--- app/routes/v1.js
+++ app/routes/v1.js
@@ -2,6 +2,7 @@ const router = require('express').Router();
 const controllers = require('../controllers');
 const asyncWrapper = require('../lib/asyncWrapper.js');
 
-router.post('/login', asyncWrapper(controllers.users.login));
+// Login API
+router.post('/user/login', asyncWrapper(controllers.users.login));
 
 module.exports = router;
